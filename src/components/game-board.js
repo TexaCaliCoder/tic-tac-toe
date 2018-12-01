e5b71a54eaa96cc4d92615a1ffd44dbd4842e8e5
TexaCaliCoder <c.roth.lambda@gmail.com>
2018-12-01 17:39:25 -0600

    bombing in the browser

--- src/components/game-board.js
+++ src/components/game-board.js
@@ -2,32 +2,36 @@ import React from 'react';
 import { GameContainer } from '../styled-components/styles';
 
 
+
 class GameBoard extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            board:Array(9).fill(''),
-            player: 'O',
+            origBoard:Array(9).fill(''),
+            user: 'O',
             ai: 'X',
+
         }
     }
     
     clicked=(e)=>{
-        const {board}=this.state
-        if (board[e.target.dataset.box] === '')
-        {e.target.innerText= "O" 
-        board[e.target.dataset.box]= 'O'
-        let newBoard = board
+        const {origBoard}=this.state
+
+        if (origBoard[e.target.dataset.box] === ''){
+        e.target.innerText= "O" 
+        origBoard[e.target.dataset.box]= 'O'
+        let newBoard = origBoard
         this.setState({
-            board: newBoard
+            origBoard: newBoard
         })
+        // this.minimax(this.state.origBoard, this.state.ai)
     }
-    else{ console.log('no!')}
     
     console.log(this.state.board)
     }
     emptySpaces=(board)=>{
-        board.filter(index=> index !== 'O' && index !== 'X' )
+        // console.log('old',board);
+        return board.filter(index=> index !== 'O' && index !== 'X' )
     }
    
      winning=(board, player)=>{
@@ -48,16 +52,53 @@ class GameBoard extends React.Component{
     }
 
     minimax = (newBoard, player) =>{
-        let availableMoves = this.emptySpaces(newBoard);
 
-        if(this.winning(newBoard, this.state.player)){
+        console.log('newboard:', newBoard)
+        const availableMoves = this.emptySpaces(newBoard);
+        let moves = [];
+        
+        if(this.winning(newBoard, this.state.user)){
             return {score: 10 }
         }else if(this.winning(newBoard, this.state.ai)){
             return {score:-10 }
-        }else if(availableMoves.length===0){
+        }else if( !availableMoves.length ){
             return {score:0}
         }
-
+        
+        
+        for(let i = 0; i < availableMoves.length; i++){
+            let move={};
+            move.index = newBoard[availableMoves[i]];
+            newBoard[availableMoves[i]]= player;
+            if(player === this.state.ai){
+                let result = this.minimax(newBoard, this.state.user);
+                move.score = result.score;
+            }else{
+                const result = this.minimax(newBoard, this.state.ai);
+                move.score=result.score
+            }
+            newBoard[availableMoves[i]] = move.index;
+            moves.push(move)
+        }
+        
+        if(player === this.state.ai){
+            let bestScore = -10000;
+            for (let i = 0; i < moves.length; i++){
+                if(moves[i].score>bestScore){
+                    bestScore=moves[i].score;
+                    this.setState({bestMove: i})
+                }
+            }
+            }else{
+                let bestScore=10000;
+                for (let i = 0; i<moves.length; i++){
+                    if(moves[i].score < bestScore){
+                        bestScore=moves[i].score;
+                        this.setState({bestMove:i})
+                    }
+                }
+                return moves[this.state.bestMove]
+            }
     }
 
     render(){
